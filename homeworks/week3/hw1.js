e02a702cabc1faaebf0cdb7c250a58bc0ed619f6
ming94114 <81964791+ming94114@users.noreply.github.com>
2021-05-24 02:22:19 +0800

    week3hw

---
+++ homeworks/week3/hw1.js
@@ -0,0 +1,21 @@
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+const lines = []
+
+rl.on('line', (line) => {
+  lines.push(line)
+})
+
+rl.on('close', () => solve(lines))
+
+function solve(lines) {
+  let result = ''
+  for (let i = 1; i <= lines; i++) {
+    result += '*'
+    console.log(result)
+  }
+}
